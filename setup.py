f452105fbaac56e78c412b504b53abf67eb631de
Roy <rhh@crosscompute.com>
2017-03-13 20:07:08 -0400

    Bump version

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ DESCRIPTION = '\n\n'.join(open(join(FOLDER, x)).read().strip() for x in [
     'README.rst'])
 setup(
     name='crosscompute-image',
-    version='0.6.7',
+    version='0.6.7.1',
     description='Image data type plugin for CrossCompute',
     long_description=DESCRIPTION,
     classifiers=[
@@ -31,6 +31,6 @@ setup(
         'pytest-runner'
     ],
     install_requires=[
-        'crosscompute>=0.6.7',
+        'crosscompute>=0.6.8',
     ],
     entry_points=ENTRY_POINTS)
